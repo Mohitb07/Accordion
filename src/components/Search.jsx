64cb7101909376293243f3fd31d71c81b71a36d4
mohitb07 <bmohit980@gmail.com>
2021-07-21 21:05:16 +0530

    request delay

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,27 +3,37 @@ import axios from 'axios'
 
 const Search = () => {
     const [term, setTerm] = useState('programming');
+    const [debouncedTerm, setDebouncedTerm] = useState(term)
     const [results, setResults] = useState([]);
 
     console.log(results)
     
     useEffect(() => {
+        const timeoutId = setTimeout(() => {
+            setDebouncedTerm(term)
+        },1000)
+
+        return () => clearTimeout(timeoutId)
+    }, [term])
+
+    useEffect(() => {
         const search = async () => {
-            const {data} = await axios.get('https://en.wikipedia.org/w/api.php', {
-                params : {
-                    action : 'query',
-                    list : 'search',
-                    origin : '*',
-                    format : 'json',
-                    srsearch : term
-                }
-            })
-
-            setResults(data.query.search)
+          const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+            params: {
+              action: 'query',
+              list: 'search',
+              origin: '*',
+              format: 'json',
+              srsearch: debouncedTerm,
+            },
+          });
+     
+          setResults(data.query.search);
+        };
+        if (debouncedTerm) {
+          search();
         }
-
-        search()
-    },[term])
+      }, [debouncedTerm]);
 
     const renderedResults = results.map((result) => {
         return (
